6d692bfc79c31c4abce0b99d8d0aa6d8f42fa4c3
Yevgeniy Alishevets <yevgalis@protonmail.com>
2018-12-23 21:03:59 +0700

    final

--- js/photo-upload.js
+++ js/photo-upload.js
@@ -3,6 +3,9 @@
 (function () {
 
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var IMAGE_WIDTH = 40;
+  var IMAGE_HEIGHT = 40;
+  var MAX_PHOTOS = 5;
   var avatarSelector = document.querySelector('.ad-form-header__input');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
   var photoSelector = document.querySelector('.ad-form__input');
@@ -38,7 +41,7 @@
     var fileName = file.name.toLowerCase();
     var previewPhotosQuantity = photoPreviewBlock.querySelectorAll('.ad-form__photo-img').length;
 
-    if (previewPhotosQuantity >= 5) {
+    if (previewPhotosQuantity >= MAX_PHOTOS) {
       return;
     }
 
@@ -46,8 +49,8 @@
       var imageItem = document.createElement('img');
 
       imageItem.alt = 'Фотография жилья';
-      imageItem.width = 40;
-      imageItem.height = 40;
+      imageItem.width = IMAGE_WIDTH;
+      imageItem.height = IMAGE_HEIGHT;
       imageItem.classList.add('ad-form__photo-img');
 
       return imageItem;
